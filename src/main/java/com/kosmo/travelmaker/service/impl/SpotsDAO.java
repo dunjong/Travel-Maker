3ed0ab760f78ecadefed9963950fef4fa89f88c4
AuthenticDisney <64462310+AuthenticDisney@users.noreply.github.com>
2020-06-25 16:37:16 +0900

    SpotsDAO

--- src/main/java/com/kosmo/travelmaker/service/impl/SpotsDAO.java
+++ src/main/java/com/kosmo/travelmaker/service/impl/SpotsDAO.java
@@ -1,5 +1,18 @@
 package com.kosmo.travelmaker.service.impl;
+import java.util.Map;
 
+import javax.annotation.Resource;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.stereotype.Repository;
+import com.kosmo.travelmaker.service.SpotsDTO;
+
+@Repository
 public class SpotsDAO {
-
+@Resource(name="template")
+private SqlSessionTemplate sqlMapper;
+	
+	public boolean saveSpot(Map map) {
+		return true;//(Integer)sqlMapper.selectOne("isLogin",map)==0 ? false : true;
+	}
 }
