4d29bd9218df630d552561e11c13f29c88500768
박종현 <jhpark9711@naver.com>
2020-07-10 11:11:45 +0900

    회원정보 리스트

--- src/main/java/com/kosmo/travelmaker/web/member/MemberController.java
+++ src/main/java/com/kosmo/travelmaker/web/member/MemberController.java
@@ -65,69 +65,33 @@ public class MemberController {
 		return "member/BookMark.tiles";
 	}
 	
-	@RequestMapping(value="MyPlannerDetails.kosmo",produces ="text/html; charset=UTF-8")
-	@ResponseBody
-	public String MyPlannerDetails(@RequestParam Map map) {
-		int planner_no=Integer.parseInt(map.get("planner_no").toString());
-
-
-		List<Map> collections = new Vector<Map>();
-		List<CitiesDTO> list_cities=cityService.selectCitiesDTO(planner_no);
-		for(CitiesDTO dto_cities:list_cities) {
-			Map<String, String> maps_cities=new HashMap<String, String>();
-			int cities_no=dto_cities.getCities_no();
-			int city_no=dto_cities.getCity_no();
-			CityDTO dto_city= cityService.selectCityDTO(city_no);
-			maps_cities.put("img", dto_city.getCity_img());
-			maps_cities.put("name", dto_city.getCity_name());
-			maps_cities.put("intro", dto_city.getCity_intro());
-			maps_cities.put("cities_no",Integer.toString(cities_no));
-			maps_cities.put("planner_no", Integer.toString(planner_no));
-			maps_cities.put("city_no",Integer.toString(city_no));
-			collections.add(maps_cities);
-		}
-		
-		return JSONArray.toJSONString(collections);
-	}
-	
-	
 	@RequestMapping("MyPlanner.kosmo")
 	public String MyPlanner(@RequestParam Map map,Model model,HttpSession session) {
 		List<PlannerDTO> list_planner=memberService.plannerList(session.getAttribute("id").toString());
 		List<Map> collections_planner = new Vector<Map>();
 		List<Map> collections_cities = new Vector<Map>();
-		List<Map<String, Integer>> list=new Vector<Map<String,Integer>>();
-		
 		for(PlannerDTO dto_planner:list_planner) {
-			Map<String ,Integer> maps=new HashMap<String, Integer>();
-			maps.put("planner_no", dto_planner.getPlanner_no());
-			maps.put("planner_acc", dto_planner.getPlanner_acc());
-			list.add(maps);
+			Map<String, String> map_planner=new HashMap<String, String>();
+			int planner_no=dto_planner.getPlanner_no();
+			int planner_acc=dto_planner.getPlanner_acc();
+			map_planner.put("planner_no", Integer.toString(planner_no));
+			map_planner.put("planner_acc", Integer.toString(planner_acc));
+			collections_planner.add(map_planner);
+			List<CitiesDTO> list_cities=cityService.selectCitiesDTO(planner_no);
+			for(CitiesDTO dto_cities:list_cities) {
+				Map<String, String> maps_cities=new HashMap<String, String>();
+				int city_no=dto_cities.getCity_no();
+				CityDTO dto_city= cityService.selectCityDTO(city_no);
+				maps_cities.put("planner_no", Integer.toString(planner_no));
+				maps_cities.put("img", dto_city.getCity_img());
+				maps_cities.put("name", dto_city.getCity_name());
+				maps_cities.put("intro", dto_city.getCity_intro());
+				collections_cities.add(maps_cities);
+			}
 		}
-		model.addAttribute("list", list);
 		
-//		for(PlannerDTO dto_planner:list_planner) {
-//			Map<String, String> map_planner=new HashMap<String, String>();
-//			int planner_no=dto_planner.getPlanner_no();
-//			int planner_acc=dto_planner.getPlanner_acc();
-//			map_planner.put("planner_no", Integer.toString(planner_no));
-//			map_planner.put("planner_acc", Integer.toString(planner_acc));
-//			collections_planner.add(map_planner);
-//			List<CitiesDTO> list_cities=cityService.selectCitiesDTO(planner_no);
-//			for(CitiesDTO dto_cities:list_cities) {
-//				Map<String, String> maps_cities=new HashMap<String, String>();
-//				int city_no=dto_cities.getCity_no();
-//				CityDTO dto_city= cityService.selectCityDTO(city_no);
-//				maps_cities.put("planner_no", Integer.toString(planner_no));
-//				maps_cities.put("img", dto_city.getCity_img());
-//				maps_cities.put("name", dto_city.getCity_name());
-//				maps_cities.put("intro", dto_city.getCity_intro());
-//				collections_cities.add(maps_cities);
-//			}
-//		}
-//		
-//		model.addAttribute("lists_planner",JSONArray.toJSONString(collections_planner));
-//		model.addAttribute("lists_cities",JSONArray.toJSONString(collections_cities));
+		model.addAttribute("lists_planner",JSONArray.toJSONString(collections_planner));
+		model.addAttribute("lists_cities",JSONArray.toJSONString(collections_cities));
 		return "member/MyPlanner.tiles";
 	}
 	
