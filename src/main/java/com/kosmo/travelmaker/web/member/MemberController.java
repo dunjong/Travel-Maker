3cf0e0443ac5c1ca1644f5f7c7ca29933bfb95d3
AuthenticDisney <64462310+AuthenticDisney@users.noreply.github.com>
2020-07-13 15:00:47 +0900

    myplanner 기진

--- src/main/java/com/kosmo/travelmaker/web/member/MemberController.java
+++ src/main/java/com/kosmo/travelmaker/web/member/MemberController.java
@@ -14,7 +14,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestMethod; 
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -23,9 +23,12 @@ import com.kosmo.travelmaker.service.CitiesDTO;
 import com.kosmo.travelmaker.service.CityDTO;
 import com.kosmo.travelmaker.service.MemberDTO;
 import com.kosmo.travelmaker.service.PlannerDTO;
+import com.kosmo.travelmaker.service.impl.AirServiceImpl;
 import com.kosmo.travelmaker.service.impl.CityServiceImpl;
+import com.kosmo.travelmaker.service.impl.HotelServiceImpl;
 import com.kosmo.travelmaker.service.impl.MemberServiceImpl;
 import com.kosmo.travelmaker.service.impl.PlannerServiceImpl;
+import com.kosmo.travelmaker.service.impl.SpotsServiceImpl;
 
 
 @Controller
@@ -39,12 +42,19 @@ public class MemberController {
 	private PlannerServiceImpl plannerService;
 	@Resource(name="cityService")
 	private CityServiceImpl cityService;
+	@Resource(name="spotsService")
+	private SpotsServiceImpl spotsService;
+	@Resource(name="hotelService")
+	private HotelServiceImpl hotelService;
+	@Resource(name="airService")
+	private AirServiceImpl airService;
 	
 
 	@RequestMapping("MyInfo.kosmo")
 	public String MyInfo(@RequestParam Map map, HttpSession session,Model model) {
 		MemberDTO dto = memberService.selectMemberDTO(session.getAttribute("id").toString());
 		model.addAttribute("id",dto.getUser_id());
+		model.addAttribute("pwd",dto.getUser_pwd());
 		model.addAttribute("name",dto.getUser_name());
 		model.addAttribute("gender",dto.getUser_gender());
 		model.addAttribute("rrn",dto.getUser_rrn());
@@ -54,8 +64,7 @@ public class MemberController {
 	public String MyInfoEdit(@RequestParam Map map, HttpSession session) {
 
 		if(memberService.updateMemberDTO(map)) {
-			
-			System.out.println(map.get("updateColumn").toString()+"수정이 완료되었습니다");
+			System.out.println("수정이 완료 되었습니다");
 		}
 		
 		return "forward:/TravelMaker/MyInfo.kosmo";
@@ -65,33 +74,92 @@ public class MemberController {
 		return "member/BookMark.tiles";
 	}
 	
+	@RequestMapping(value="MyPlannerDetails.kosmo",produces ="text/html; charset=UTF-8")
+	@ResponseBody
+	public String MyPlannerDetails(@RequestParam Map map) {
+		int planner_no=Integer.parseInt(map.get("planner_no").toString());
+
+
+		List<Map> collections = new Vector<Map>();
+		List<CitiesDTO> list_cities=cityService.selectCitiesDTO(planner_no);
+		for(CitiesDTO dto_cities:list_cities) {
+			Map<String, String> maps_cities=new HashMap<String, String>();
+			int cities_no=dto_cities.getCities_no();
+			int city_no=dto_cities.getCity_no();
+			CityDTO dto_city= cityService.selectCityDTO(city_no);
+			maps_cities.put("img", dto_city.getCity_img());
+			maps_cities.put("name", dto_city.getCity_name());
+			maps_cities.put("intro", dto_city.getCity_intro());
+			maps_cities.put("cities_no",Integer.toString(cities_no));
+			maps_cities.put("planner_no", Integer.toString(planner_no));
+			maps_cities.put("city_no",Integer.toString(city_no));
+			collections.add(maps_cities);
+		}
+		
+		return JSONArray.toJSONString(collections);
+	}
+	
+	
+	@RequestMapping(value="MyPlannerDelete.kosmo",produces ="text/html; charset=UTF-8")
+	@ResponseBody
+	public String MyPlannerDelete(@RequestParam Map map) {
+		int planner_no=Integer.parseInt(map.get("planner_no").toString());
+		List<CitiesDTO> cities_dto_list= plannerService.selectCitiesDTOList(planner_no);
+		for(CitiesDTO cities_dto:cities_dto_list) {
+			int cities_no=cities_dto.getCities_no();
+			System.out.println("cities_no: "+cities_no);
+			List<Integer> plan_no_list=plannerService.selectPlanNoByCitiesNo(cities_no);
+			for(int plan_no:plan_no_list ){
+				System.out.println("plan_no: "+plan_no);
+				if(spotsService.deleteSpotByPlanNo(plan_no)) {
+					System.out.println("spot들 삭제 완료");
+					
+				};
+			}
+			if(plannerService.deletePlanByCitiesNo(cities_no)){
+				System.out.println("plan들 삭제 완료");
+				
+			};
+			if(hotelService.deleteHotelByCitiesNo(cities_no)){
+				System.out.println("호텔들 삭제 완료");
+				
+			};
+			
+			
+			
+		}
+		
+		if(plannerService.deleteCitiesByPlannerNo(planner_no)) {
+			System.out.println("Cities들 삭제 완료");
+			
+		};
+		if(airService.deleteResByCitiesNo(planner_no)){
+			System.out.println("항공권예약들 삭제 완료");
+			
+		};
+		
+		
+		if(plannerService.deletePlannerByNo(planner_no)) {
+			System.out.println("planner 삭제 완료");
+		}
+		
+		
+		
+		return "삭제 성공";
+	}
+	//변경됨
 	@RequestMapping("MyPlanner.kosmo")
 	public String MyPlanner(@RequestParam Map map,Model model,HttpSession session) {
 		List<PlannerDTO> list_planner=memberService.plannerList(session.getAttribute("id").toString());
-		List<Map> collections_planner = new Vector<Map>();
-		List<Map> collections_cities = new Vector<Map>();
-		for(PlannerDTO dto_planner:list_planner) {
-			Map<String, String> map_planner=new HashMap<String, String>();
-			int planner_no=dto_planner.getPlanner_no();
-			int planner_acc=dto_planner.getPlanner_acc();
-			map_planner.put("planner_no", Integer.toString(planner_no));
-			map_planner.put("planner_acc", Integer.toString(planner_acc));
-			collections_planner.add(map_planner);
-			List<CitiesDTO> list_cities=cityService.selectCitiesDTO(planner_no);
-			for(CitiesDTO dto_cities:list_cities) {
-				Map<String, String> maps_cities=new HashMap<String, String>();
-				int city_no=dto_cities.getCity_no();
-				CityDTO dto_city= cityService.selectCityDTO(city_no);
-				maps_cities.put("planner_no", Integer.toString(planner_no));
-				maps_cities.put("img", dto_city.getCity_img());
-				maps_cities.put("name", dto_city.getCity_name());
-				maps_cities.put("intro", dto_city.getCity_intro());
-				collections_cities.add(maps_cities);
-			}
-		}
-		
-		model.addAttribute("lists_planner",JSONArray.toJSONString(collections_planner));
-		model.addAttribute("lists_cities",JSONArray.toJSONString(collections_cities));
+	      List<Map<String, Integer>> list=new Vector<Map<String,Integer>>();
+	      
+	      for(PlannerDTO dto_planner:list_planner) {
+	         Map<String ,Integer> maps=new HashMap<String, Integer>();
+	         maps.put("planner_no", dto_planner.getPlanner_no());
+	         maps.put("planner_acc", dto_planner.getPlanner_acc());
+	         list.add(maps);
+	      }
+	      model.addAttribute("list", list);
 		return "member/MyPlanner.tiles";
 	}
 	
@@ -112,22 +180,6 @@ public class MemberController {
 			session.setAttribute("id", dto.getUser_id());
 		}
 		return "/home.tiles";
-	}
-	@ResponseBody
-	@RequestMapping("kakao.do")
-	public String KSignUp(MemberDTO dto,HttpSession session) {
-		if(memberService.idCheck(dto.getUser_id())=="failure"){
-			session.setAttribute("id", dto.getUser_id());
-			return "login";
-		}
-		if(dto.getUser_gender()==null) dto.setUser_gender("male");
-		if(dto.getUser_name()==null) dto.setUser_name("이름없음");
-		if(dto.getUser_rrn()==null) dto.setUser_rrn("0");
-		if(memberService.SignUp(dto)) { 
-			session.setAttribute("id", dto.getUser_id());
-			return "signup";
-		}
-		return "failure";
 	}
 	@RequestMapping("IdCheck.do")
 	@ResponseBody
