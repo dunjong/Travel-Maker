9a71f74f212d04e40033f075e1876b5f5151807b
20203333 <64462374+20203333@users.noreply.github.com>
2020-07-23 05:32:43 +0900

    기진 수정 최종

--- src/main/java/com/kosmo/travelmaker/service/impl/PlannerServiceImpl.java
+++ src/main/java/com/kosmo/travelmaker/service/impl/PlannerServiceImpl.java
@@ -213,5 +213,11 @@ public class PlannerServiceImpl implements PlannerService{
 		return plannerDAO.selectAccNosByPlannerNo(planner_no);
 	}
 
+	@Override
+	public boolean updateResOk(String h_a_no) {
+		// TODO Auto-generated method stub
+		return plannerDAO.updateResOk(h_a_no);
+	}
+
 	
 }
