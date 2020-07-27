9382836f13cece865974eb47cb3b719cc3063df6
박종현 <jhpark9711@naver.com>
2020-07-27 10:16:20 +0900

    z

---
+++ src/main/java/com/kosmo/travelmaker/service/Adminservice.java
@@ -0,0 +1,17 @@
+package com.kosmo.travelmaker.service;
+
+import org.springframework.stereotype.Repository;
+import javax.servlet.http.HttpSession;
+ 
+
+public interface Adminservice {
+ 
+    
+    static void admin_member_forced_evictionCheck(MemberDTO dto) throws Exception {
+		// TODO Auto-generated method stub
+		
+	} //강제탈퇴 시킬때 해당 회원이 있는지 체크하는 메소드
+ 
+    
+}
+ 
