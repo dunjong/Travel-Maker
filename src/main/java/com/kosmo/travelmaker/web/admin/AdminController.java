74aa450b09b5bbb4c34618056c6cc7887fd37a8d
AuthenticDisney <64462310+AuthenticDisney@users.noreply.github.com>
2020-07-29 12:51:47 +0900

    빠빠빨간줄 궁금해 허니

--- src/main/java/com/kosmo/travelmaker/web/admin/AdminController.java
+++ src/main/java/com/kosmo/travelmaker/web/admin/AdminController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.kosmo.travelmaker.service.Adminservice;
 import com.kosmo.travelmaker.service.MemberDTO;
 import com.kosmo.travelmaker.service.ReviewDTO;
 import com.kosmo.travelmaker.service.impl.MemberServiceImpl;
